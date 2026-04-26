56b5331922156bd65d0c767e8b9f2d03c3ed2e4b
Ayushiagrawal2406 <aa8030@srmist.edu.in>
2026-04-26 14:42:00 +0530

    Added UC3: User input handling

---
+++ UC3_UserInput.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class UC3_UserInput {
+
+    public static int getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a slot number (1-9): ");
+        int slot = scanner.nextInt();
+
+        return slot;
+    }
+
+    public static void main(String[] args) {
+        int userSlot = getUserInput();
+
+        System.out.println("You selected slot: " + userSlot);
+    }
+}
